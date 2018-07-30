a8f8dccfb8edc24d61d888f390da5b68e39b2f99
naynapashilkar <nayna.pashilkar@gmail.com>
2018-07-30 18:44:54 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,2 +1,21 @@
 'use-strict';
 //add your code here
+function h1Selector () {
+ return $('h1')
+}
+
+function liOlSelector () {
+  return $('ol').find('li')
+}
+
+function linkSelector () {
+  return $('.box5#box4 a')
+}
+
+function imageSelector () {
+  return $("[alt*='cat-sleeping']")
+}
+
+function checkboxInputSelector () {
+  
+}
